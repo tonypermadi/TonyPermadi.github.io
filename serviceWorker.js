ef3ca2a2e5e5020d7eb768f67ae0cc90e17e4694
Tony Permadi <61304298+tonypermadi@users.noreply.github.com>
2020-03-04 03:18:28 +0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -13,22 +13,3 @@ event.waitUntil(onInstall(event));
 });
 self.addEventListener('activate', event => {
 });
-self.addEventListener('fetch', (event) => {
-if (event.request.method === 'GET') {
-event.respondWith(
-caches.match(event.request)
-.then((cached) => {
-var networked = fetch(event.request)
-.then((response) => {
-let cacheCopy = response.clone()
-caches.open(CACHE_NAME)
-.then(cache => cache.put(event.request, cacheCopy))
-return response;
-})
-.catch(() => caches.match(offlinePage));
-return cached || networked;
-})
-)
-}
-return;
-});
