fb9cb660fc48bf57a74d31ea0ac6a9b12fc6846b
Tony Permadi <61304298+tonypermadi@users.noreply.github.com>
2020-03-04 03:02:19 +0700

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,82 +1,26 @@
-self.addEventListener('fetch', function fetcher (event) {
-  var request = event.request;
-  if (request.method !== 'GET') {
-    event.respondWith(fetch(request)); return;
-  }
+let CACHE_NAME = 'sw-v1'
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll('https://res.cloudinary.com/tony-permadi/image/upload/tpimg.webp'))
+)
+})
+self.addEventListener('fetch', (event) => {
+if (event.request.method === 'GET') {
+event.respondWith(
+caches.match(event.request)
+.then((cached) => {
+var networked = fetch(event.request)
+.then((response) => {
+let cacheCopy = response.clone()
+caches.open(CACHE_NAME)
+.then(cache => cache.put(event.request, cacheCopy))
+return response;
+})
+.catch(() => caches.match(offlinePage));
+return cached || networked;
+})
+)
+}
+return;
 });
-
-event.respondWith(caches
-  .match(request) 
-  .then(queriedCache)
-);
-
-function queriedCache (cached) {
-  var networked = fetch(request)
-    .then(fetchedFromNetwork, unableToResolve)
-    .catch(unableToResolve);
-  return cached || networked;
-}
-
-function fetchedFromNetwork (response) {
-  var clonedResponse = response.clone();
-  caches.open(version + 'pages').then(function add (cache) {
-    cache.put(request, clonedResponse);
-  });
-  return response;
-}
-
-function unableToResolve () {
-  return offlineResponse();
-}
-function offlineResponse () {
-  return new Response('', { status: 503, statusText: 'Service Unavailable' });
-}
-
-function unableToResolve () {
-  var accepts = request.headers.get('Accept');
-  if (accepts.indexOf('image') !== -1) {
-    return caches.match(rainbows);
-  }
-  return offlineResponse();
-}
-
-function unableToResolve () {
-  var url = new URL(request.url);
-  var accepts = request.headers.get('Accept');
-  if (accepts.indexOf('image') !== -1) {
-    if (url.host === 'www.gravatar.com') {
-      return caches.match(mysteryMan);
-    }
-    return caches.match(rainbows);
-  }
-  return offlineResponse();
-}
-
-function unableToResolve () {
-  var url = new URL(request.url);
-  var accepts = request.headers.get('Accept');
-  if (accepts.indexOf('image') !== -1) {
-    if (url.host === 'www.gravatar.com') {
-      return caches.match(mysteryMan);
-    }
-    return caches.match(rainbows);
-  }
-  if (url.origin === location.origin) {
-    return caches.match('/offline');
-  }
-  return offlineResponse();
-}
-
-if (url.origin === location.origin && accepts.indexOf('application/json') !== -1) {
-  return offlineView();
-}
-function offlineView () {
-  var viewModel = {
-    model: { action: 'error/offline' }
-  };
-  var options = {
-    status: 200,
-    headers: new Headers({ 'content-type': 'application/json' })
-  };
-  return new Response(JSON.stringify(viewModel), options);
-}
